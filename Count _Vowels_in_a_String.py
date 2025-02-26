effe1d2e63198cfd87327b1763c75941674973f2
LeenaAlhabsi <leenahamoodalhabsi@gmail.com>
2025-02-26 11:59:27 +0400

    chick for valid input

--- Count _Vowels_in_a_String.py
+++ Count _Vowels_in_a_String.py
@@ -1,7 +1,14 @@
 print("welcome to the Vowel Counter App") #start of the program
 
-input_string = input("\nEnter a word or phrase: ")#get the input string from the user
-input_string1 = input_string.lower() #convert the input string to lowercase
+program = True
+while program:
+   input_string = input("\nEnter a word or phrase: ")#get the input string from the user
+   if input_string == "": #check if the input string is empty
+    print("\nPlease enter a non-empty string") #print a message to the user
+    print("Try again!") #print a message to the user
+   else:
+    input_string1 = input_string.lower() #convert the input string to lowercase
+    program = False
 
 vowels = ['a', 'e', 'i', 'o', 'u'] #list of vowels
 vowels_count = 0 #variable to count the vowels
